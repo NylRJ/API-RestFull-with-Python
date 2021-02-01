feee30113940916ffd1d9c76b3d68f47a7223c4d
Moises RZ <loveloungeoficial@gmail.com>
2021-01-31 22:35:28 -0300

    Customizing data serialization

--- cursos/serializers.py
+++ cursos/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.db.models import Avg
 
 from .models import Curso, Avaliacao
 
@@ -22,7 +23,7 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
         )
 
     def validate_avaliacao(self, valor):
-        if valor in range(1, 6): # 1, 2, 3, 4, 5
+        if valor in range(1, 6):  # 1, 2, 3, 4, 5
             return valor
         raise serializers.ValidationError('A avaliação precisa ser um inteiro entre 1 e 5')
 
@@ -37,6 +38,8 @@ class CursoSerializer(serializers.ModelSerializer):
     # Primary Key Releted Field
     avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
+    media_avaliacoes = serializers.SerializerMethodField()
+
     class Meta:
         model = Curso
         fields = (
@@ -45,5 +48,15 @@ class CursoSerializer(serializers.ModelSerializer):
             'url',
             'criacao',
             'ativo',
-            'avaliacoes'
+            'avaliacoes',
+            'media_avaliacoes'
         )
+
+    def get_media_avaliacoes(self, obj):
+        media = obj.avaliacoes.aggregate(Avg('avaliacao')).get('avaliacao__avg')
+
+        if media is None:
+            return 0
+        return round(media * 2) / 2
+
+
